0a714a360714cb07f2a1bb7fb564dae0ade9651e
darshgah <darshikaah@gmail.com>
2025-05-17 11:22:36 +0400

    Update BaseClass.java

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -23,74 +23,82 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public class BaseClass {
-	
-	public static WebDriver driver;
-	public Logger logger;
-	public Properties p;
-	
-	@BeforeClass
-	@Parameters({"browser"})
-	public void setup(String br) throws IOException, InterruptedException {
-		
-		// Load config file
-		FileInputStream file = new FileInputStream("./src/test/resources/config.properties");
-		p = new Properties();
-		p.load(file);
-		
-		// Initialize logger
-		logger = LogManager.getLogger(this.getClass());
-		
-		// Initialize browser drivers
-		switch (br.toLowerCase()) {
-			case "chrome":
-				ChromeOptions chromeOptions = new ChromeOptions();
-			            chromeOptions.addArguments("--no-sandbox");
-			            chromeOptions.addArguments("--disable-dev-shm-usage");
-			            chromeOptions.addArguments("--disable-gpu");
-			            chromeOptions.addArguments("--disable-extensions");
-			            chromeOptions.addArguments("--remote-allow-origins=*"); // optional for newer Chrome
-			            driver = new ChromeDriver(chromeOptions);
-			            break;
-				
-			case "edge":
-				driver = new EdgeDriver();
-				break;
-				
-			case "ff":
-				driver = new FirefoxDriver();
-				break;
-				
-			default:
-				System.out.println("Invalid browser: " + br);
-				return;
-		}
-		
-		// Common setup
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get(p.getProperty("url"));
-		driver.manage().window().maximize();
-		Thread.sleep(3000);
-	}
-	
-	@AfterClass
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();  
-		}
-	}
-	
-	// Method to capture screenshot
-	public String captureScreen(String tname) throws IOException {
-		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		String scrfolder = System.getProperty("user.dir") + File.separator + "screenshots";
-		
-		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-		
-		String targetFilePath = scrfolder + File.separator + tname + "_" + timeStamp + ".png";
-		File targetFile = new File(targetFilePath);
-		FileUtils.copyFile(sourceFile, targetFile);
-		
-		return targetFilePath;
-	}
+
+    public static WebDriver driver;
+    public Logger logger;
+    public Properties p;
+
+    @BeforeClass
+    @Parameters({"browser"})
+    public void setup(String br) throws IOException, InterruptedException {
+
+        // Load config file
+        FileInputStream file = new FileInputStream("./src/test/resources/config.properties");
+        p = new Properties();
+        p.load(file);
+
+        // Initialize logger
+        logger = LogManager.getLogger(this.getClass());
+
+        // Initialize browser drivers
+        switch (br.toLowerCase()) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--disable-extensions");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+
+                
+                String userDataDir = System.getProperty("java.io.tmpdir") + "/chrome-profile-" + System.currentTimeMillis();
+                chromeOptions.addArguments("--user-data-dir=" + userDataDir);
+
+                // Optional headless mode if needed in CI
+                 chromeOptions.addArguments("--headless=new");
+
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+
+            case "ff":
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Invalid browser: " + br);
+                return;
+        }
+
+        // Common setup
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(p.getProperty("url"));
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    // Method to capture screenshot
+    public String captureScreen(String tname) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String scrfolder = System.getProperty("user.dir") + File.separator + "screenshots";
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String targetFilePath = scrfolder + File.separator + tname + "_" + timeStamp + ".png";
+        File targetFile = new File(targetFilePath);
+        FileUtils.copyFile(sourceFile, targetFile);
+
+        return targetFilePath;
+    }
 }
